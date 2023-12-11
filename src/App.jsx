4e8416cc2ee5a6c8ff17cdb271fee0af59057309
Basharat khan <bk173498@gmail.com>
2023-12-11 17:49:03 +0530

    you

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ const App = () => {
     ADD_OPERATOR: "ADD_OPERATOR",
     CALCULATION: "CALCULATION"
   }
-
   const reducer = (state, { type, payload }) => {
-
     const calculation = ({ calValue, displayValue, operator }) => {
       let cal = parseFloat(calValue)
       let dis = parseFloat(displayValue)
@@ -32,72 +30,102 @@ const App = () => {
     switch (type) {
       case ACTIONS.ADD_DIGITS:
         if (payload === "0" && state.displayValue === "0") return state
-        if (payload === "." && !state.calValue) return state
+        if (payload === "." && state.displayValue === "0") {
+          return {
+            ...state,
+            displayValue: `${state.displayValue || ""}${payload}`
+          }
+        }
+        if (payload === "." && state.calValue && !state.displayValue) return state
         if (payload === "." && state.displayValue.includes(".")) return state
+
+        if (state.displayValue === "0") {
+          return {
+            ...state,
+            displayValue: payload
+          }
+        }
         return {
           ...state,
           displayValue: `${state.displayValue || ""}${payload}`
 
         }
       case ACTIONS.ADD_OPERATOR:
-        if (state.displayValue === "0" && state.calValue && state.operator) {
+        if (state.displayValue && !state.calValue) {
           return {
-            ...state,
+            calValue: state.displayValue,
             operator: payload
           }
         }
-        if (state.calValue && state.operator === payload) {
+        if (payload === state.operator && state.displayValue) {
           return {
-            operator: payload,
             calValue: calculation(state),
-            displayValue: "0",
-          }
-        }
-        if (!state.calValue && state.displayValue) {
-          return {
-            operator: payload,
-            calValue: state.displayValue,
-            displayValue: "0",
-          }
-        }
-        if (state.operator && state.operator !== payload) {
-          return {
-            ...state,
-            operator: payload,
-            displayValue: "0"
+            operator: state.operator,
+            displayValue: ""
           }
         }
 
-
+        if (payload !== state.operator && state.displayValue) {
+          return {
+            calValue: calculation(state),
+            operator: payload,
+            displayValue: ""
+          }
+        }
         return state
 
       case ACTIONS.CALCULATION:
+        if (!state.calValue) return state
+        if (!state.displayValue) return state
         return {
           calValue: calculation(state),
           operator: state.operator,
           displayValue: ""
         }
-
       case ACTIONS.ALL_CLEAR:
-        return {}
-
-      case ACTIONS.DELETE_DIGIT:
-        if (state.displayValue) {
+        if (!state.calValue) {
           return {
-            ...state,
-            displayValue: state.displayValue == "0" ? "0" : state.displayValue.length == 1 ? "0" : state.displayValue.slice(0, -1)
+            displayValue: "0",
+            operator: null,
           }
         }
-        if (!state.displayValue) {
+        if (state.calValue) {
+          return {
+            displayValue: "0",
+            calValue: undefined,
+            operator: null,
+          }
+        }
+        if (state.displayValue) {
+          return {
+            displayValue: "0",
+            calValue: undefined,
+            operator: null,
+          }
+        }
+      case ACTIONS.DELETE_DIGIT:
+        if (state.displayValue === "0") {
           return {
             displayValue: "0"
           }
         }
-        return state
+        else {
+          if (!state.displayValue) return state
+          if (state.displayValue.length == 1) {
+            return {
+              ...state,
+              displayValue: "0"
+            }
+          }
+          return {
+            ...state,
+            displayValue: state.displayValue.slice(0, -1)
+          }
+        }
     }
   }
 
-  const [{ displayValue = "0", calValue, operator }, dispatch] = useReducer(reducer, {})
+  const [{ displayValue, calValue, operator }, dispatch] = useReducer(reducer, { displayValue: "0" })
   return (
     <div className='container'>
       <div className="calculator">
